7863b1e345da91c159eae868e768fbf104ed71bc
Steven Williams <evilgeniusnyc@me.com>
2020-02-26 03:21:15 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,19 +3,7 @@
 array_of_colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def disolay_rainbow(array_of_colors)
-puts "R: #{red}[0]"
-puts "O: #{orange}[1]"
-puts "Y: #{yellow}[2]"
-puts "G: #{green}[3]"
-puts "B: #{blue}[4]"
-puts "I: #{indigo}[5]"
-puts "V: #{violet}[6]"
-
-end
-
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-
-def display_rainbow(colors)
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+ puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end 
+
 
